a2d1d08c41a215d0a2f64a7b55d59a338e1e956b
Evan Soroken <esoroken@yahoo.com>
2022-02-24 11:28:37 -0500

    Compiles, needs logging

--- wifi.cc
+++ wifi.cc
@@ -6,6 +6,7 @@
 #include "ns3/csma-module.h"
 #include "ns3/mobility-module.h"
 #include "ns3/yans-wifi-helper.h"
+#include "ns3/ssid.h"
 
 
 using namespace ns3;
@@ -17,21 +18,18 @@ int main(int argc, char* argv[])
   //first define nodes, bare devices on the network
   //NodeContainer is an easy way to manage our nodes
   NodeContainer wifiNodes;
-  wifiNnodes.Create(4);
+  wifiNodes.Create(4);
 
-  NetDeviceContainer apNodes;
-  apNodes.create(1);
+  NodeContainer wifiApNode;
+  wifiApNode.Create(1);
+
 
   //Installing an internet stack onto the nodes so they are networked enabled (IP, TCP, UDP, etc.)
   InternetStackHelper stack;
   //The stack helper takes the node container we made above as an argument
   stack.Install(wifiNodes);
-  stack.Install(apNodes);
+  stack.Install(wifiApNode);
 
-  Ipv4AddressHelper addr;
-  addr.SetBase("192.168.7.0", "255.255.255.0");
-  addr.Assign(apNodes);
-  addr.Assign(wifiNodes);
 
   /* While not used in this WiFi demo, we could init a p2p connect between 2 nodes by initing a helper and settings its data transfer rate and delay
   PointToPointHelper p2p;
@@ -44,15 +42,14 @@ int main(int argc, char* argv[])
   devices = p2p.Install(nodes);
   */
 
-  //Just like the p2p example, we can also manuyally set the data rate and delay, however this constructor comes with default values
+  //Just like the p2p example, we can also manually set the data rate and delay, however this constructor comes with default values
   YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
   //Init a generic layer 1 implementation
-  YansWifiPhyHelper phy = YansWifiPhyHelper::Default();
+  YansWifiPhyHelper phy;
   phy.SetChannel(channel.Create());
 
   //Create wifi object and set the rate adaptation algorithm (think, performance)
   WifiHelper wifi;
-  wifi.SetRemoteStationManager("ns3:AarfWifiManager");
 
   //layer 2 initialization
   WifiMacHelper mac;
@@ -65,9 +62,14 @@ int main(int argc, char* argv[])
   stations = wifi.Install(phy, mac, wifiNodes);
 
   //Reset mac values for access point deployment, since this is an AP, it doesnt probe
-  mac.SetType("ns3:ApWifiMac", "Ssid", SsidValue(ssid));
+  mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
   NetDeviceContainer aps;
-  aps = wifi.Install(phy, mac, apNodes);
+  aps = wifi.Install(phy, mac, wifiApNode);
+
+  Ipv4AddressHelper addr;
+  addr.SetBase("192.168.7.0", "255.255.255.0");
+  addr.Assign(stations);
+  addr.Assign(aps);
 
   //We get ready to define the mobility of the nodes, since this is a wifi network
   MobilityHelper mobility;
@@ -82,14 +84,18 @@ int main(int argc, char* argv[])
       "LayoutType", StringValue ("RowFirst"));
 
   mobility.SetMobilityModel("ns3::RandomWalk2dMobilityModel",
-      "Bounds", RectangeValue(Rectangle(-100,100 -100,100)));
+      "Bounds", RectangleValue(Rectangle(-50, 50, -50, 50)));
 
   mobility.Install(wifiNodes);
 
   mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-  mobility.Install(apNodes);
+  mobility.Install(wifiApNode);
 
-  IPv4GlobalRoutingHelper::PopulateRoutingTables();
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
+  Simulator::Stop(Seconds(10.0));
+
+  Simulator::Run();
+  Simulator::Destroy();
   return 0;
 }
